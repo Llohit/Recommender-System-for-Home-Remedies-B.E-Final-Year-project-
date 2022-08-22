af17144e117d83665f5573bc2f837ce4b39962b7
Llohit <91447122+Llohit@users.noreply.github.com>
2022-08-22 20:28:22 +0530

    Add files via upload

---
+++ Recommender-System-For-Home-Remedy-2f4f000c21a081d9579529fcde2b61baa7c2ee0c/beproj/Cranberry.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>HousedHealth</title>
+    <link rel="icon" href="https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-hospital-medical-kiranshastry-lineal-color-kiranshastry-2.png"/>
+    <link rel="stylesheet" href="CSS/CategoryOnClick.css">
+</head>
+<body>
+    <h1>Cranberry</h1><br><br><br><br><br><br>
+       <pre style="color: #8B0000">
+            Cranberries are a rich source of several vitamins and minerals, especially
+            Vitamin C. They contain various antioxidants that may be
+            beneficial for heart health.
+            It can be used to cure Diabetes, Cold, Flu and Urine Odor, etc.
+            The main nutrients in 1 cup (100 grams) of raw, unsweetened cranberries are:
+
+            Calories: 46
+            Water: 87%
+            Protein: 0.4 grams
+            Carbs: 12.2 grams
+            Sugar: 4 grams
+            Fiber: 4.6 grams
+            Fat: 0.1 grams
+       </pre><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+   
+    <div class="textOverImage" style='background-image: url("https://4.imimg.com/data4/SD/VN/MY-10055970/fresh-cranberry-500x500.jpg");'></div>
+</body>
+</html>
